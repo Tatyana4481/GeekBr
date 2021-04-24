7fcfc5e911a1f3900688e53e06a5446bbc636ad3
Tatyana <tata_sk@mail.ru>
2021-04-25 02:52:17 +0700

    Point stop

---
+++ lesson-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Как ваше имя? "); ;
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now}.");
+        }
+    }
+}
